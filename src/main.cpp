13659318f4b6f067d3aceb0633cdc76f2ba8b79f
Brian Le <bml2@augusta.cs.pdx.edu>
2020-02-18 13:33:08 -0800

    added an overall structure for my IO

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,96 @@
 //code goes here
+/*
+
+  code planning for IO 
+ 
+cout welcome to the program
+cout would you like to log in as a provider or manager
+cin answer
+
+
+if manager:
+	-
+if provider:
+
+
+loop: 
+	1.) ask to input ID
+		- if invalid 
+			beginning of loop again
+		- elseif correct
+			doctor = new provider(inputted_ID)		
+
+loop:
+	cout: options to choose: 
+		- provider directory  (1)
+		- bill choc An (2)
+		- modify member account (3)
+		-modify provider account (4)
+cin answer ( in numbers)
+
+if (1)
+	display provider directory
+if (2)
+	cout please enter member code
+	cin code in a temp variable
+	loop
+		cout provider directory	
+		cout : enter 6 digit service code
+		cin 6 digit code
+		cout if this is right or not
+		if not correct
+			go back loop
+		if correct
+			exit loop
+		cout enter comments?
+		cin comments
+		
+		save all inputs
+
+		cout to enter following things
+		cin date & time
+		cin date service was provided
+		cin member name and #
+		cin service code
+		cin service fee
+		
+		save all in object?
+		
+if (3)
+	display :
+		- add (1)
+		- delete (2)
+		-modify(3)
+	cin answer (in numbers)
+	
+	if (1)
+		- adding member
+	if (2)
+		- deleting member
+	if (3)
+		- access member account and modify
+
+if (4)
+	display :
+		- add (1)
+		- delete (2)
+		-modify(3)
+	cin answer (in numbers)
+	
+	if (1)
+		- adding provider
+	if (2)
+		- deleting provider
+	if (3)
+		- access provider account and modify
+
+
+cout would user like to do more functions
+cin answer
+if yes
+	loop
+if no
+	exit
+
+
+*/
